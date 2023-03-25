65ed768ddca76d5e9b56e1cda920f0659de24edf
sametfatih <samet.fatih.gozum@hotmail.com>
2023-03-26 01:09:12 +0300

    db creted

---
+++ Infrastructure/alsatcomAPI.Persistence/Contexts/AlsatcomDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alsatcomAPI.Persistence.Contexts
+{
+    public class AlsatcomDbContext : DbContext
+    {
+        public AlsatcomDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+    }
+}
---
+++ Infrastructure/alsatcomAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using alsatcomAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alsatcomAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<AlsatcomDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString));
+        }
+    }
+}
